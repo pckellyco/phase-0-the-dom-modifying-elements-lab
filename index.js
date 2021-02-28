930df31b08c13ed9f82b7172d736202875db15ac
Paige Kelly <pckelly.co@gmail.com>
2021-02-28 16:48:49 -0700

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// Write your code here!
+const element = document.createElement("div");
+document.body.appendChild(element);
+
+const mainElement = document.getElementById("main");
+mainElement.parentNode.removeChild(mainElement)
+
+const newHeader = document.createElement("h1");
+document.body.appendChild(newHeader);
+
+newHeader.id = "victory"
+
+document.getElementById("victory").innerHTML = "Paige is the champion";
+
